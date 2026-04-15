9377cd26d1d857e31d37a0b9cc52eadc6bb5e888
Catlen <catlenfreitas@outlook.com>
2026-04-15 14:25:16 -0300

    ajuste porta para deploy

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.post('/login', (req, res) => {
   });
 });
 
-// INICIAR SERVIDOR
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando');
 });
